100b3449a2928aab4101cdf5d8bd99537612b57a
mebiusbox <mebiusbox@gmail.com>
2023-01-09 19:23:09 +0900

    set deploymentBranch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   organizationName: 'mebiusbox', // Usually your GitHub org/user name.
   projectName: 'docusaurus-demo.github.io', // Usually your repo name.
   trailingSlash: false,
+  deploymentBranch: 'main',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
