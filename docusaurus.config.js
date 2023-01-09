032e9ff0038af5c19827675790f2e02a50e23d3c
mebiusbox <mebiusbox@gmail.com>
2023-01-09 19:25:50 +0900

    fix projectName

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mebiusbox', // Usually your GitHub org/user name.
-  projectName: 'docusaurus-demo.github.io', // Usually your repo name.
+  projectName: 'docusaurus-demo', // Usually your repo name.
   trailingSlash: false,
   deploymentBranch: 'main',
 
